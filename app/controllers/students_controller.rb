35f8c2c58acc35986d4573c92fd575e21e1535a9
golivett <golivett@citadel.edu>
2015-11-21 11:29:52 +0000

    Iteration 2

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -67,7 +67,8 @@ class StudentsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_student
-      @student = Student.find(params[:q])
+      id = params[:q]
+      @student = Student.find(id)
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
